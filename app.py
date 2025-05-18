9559d5f979e0f91e73c7370c2ea3bc12b7b80a20
Shuvo <sahashuvo90@gmail.com>
2025-05-18 18:56:34 -0400

    Add files via upload
    Just a sample py file to check.

---
+++ app.py
@@ -0,0 +1,7 @@
+Employee_name = ("Shuvo Saha")
+Employee_age = 34
+is_new_Employee = False
+
+print ("Employee Name: ", Employee_name)
+print ("Employee Age: ", Employee_age)
+print ("New Employee: ", is_new_Employee)
